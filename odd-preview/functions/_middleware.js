657f1c0f86cb60b782ac97a2200b021c5629edd6
ArewsAi1 <aj@tmclife.net>
2026-08-02 11:59:14 -0600

    Inject Weebly restoration CSS into every live HTML response

--- odd-preview/functions/_middleware.js
+++ odd-preview/functions/_middleware.js
@@ -1,8 +1,25 @@
+class HeadInjector {
+  element(element) {
+    element.append('<link rel="stylesheet" href="/weebly-restore.css">', { html: true });
+  }
+}
+
 export async function onRequest(context) {
   const url = new URL(context.request.url);
+
   if (url.hostname === 'ogdendeckdepot.com') {
     url.hostname = 'www.ogdendeckdepot.com';
     return Response.redirect(url.toString(), 301);
   }
-  return context.next();
+
+  const response = await context.next();
+  const contentType = response.headers.get('content-type') || '';
+
+  if (!contentType.includes('text/html')) {
+    return response;
+  }
+
+  return new HTMLRewriter()
+    .on('head', new HeadInjector())
+    .transform(response);
 }
